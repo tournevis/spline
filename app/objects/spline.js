8496928a451f8ff29fd1d2107c2caf190b9cf0a9
tournevis <arthur.ro92@gmail.com>
2016-02-28 19:35:47 +0100

    :snowflake: merci jojo :snowflake:

--- app/objects/spline.js
+++ app/objects/spline.js
@@ -25,11 +25,31 @@ export default class Spline extends THREE.Object3D {
     this.geometry.vertices = this.curve.getPoints( this.points.length-1 );
     this.material = new THREE.LineBasicMaterial({ color : 0x00FFBF, linewidth: 10, vertexColors: THREE.VertexColors });
 
+
     //Create the final Object3d to add to the scene
     this.splineObject = new THREE.Line( this.geometry, this.material );
+
+    this.tubeGeo = new THREE.TubeGeometry( this.curve,  //path
+        20,    //segments
+        10,     //radius
+        8,     //radiusSegments
+        false  //closed
+    );
+    this.tubeMat = new THREE.MeshBasicMaterial({
+					color: 0xFF0000,
+					opacity: 0.3,
+					wireframe: true,
+					transparent: true
+			})
+    this.tubeMesh = new THREE.Mesh(this.tubeGeo, this.tubeMat);
+    this.add(this.tubeMesh);
     this.add(this.splineObject);
+
+    this.curve.dynamic = true;
     this.splineObject.dynamic = true;
-    console.log(this.splineObject);
+
+  //  console.log(this.splineObject);
+//  console.log(this.curve);
   }
 
   soundData(data){
@@ -38,21 +58,26 @@ export default class Spline extends THREE.Object3D {
   }
 
   update() {
+    this.tubeMesh.geometry.verticesNeedUpdate = true;
+    this.tubeMesh.parent.curve.needsUpdate = true;
     this.splineObject.geometry.verticesNeedUpdate = true;
 
 
     for (var i = 0; i < this.myData.length; i++) {
-      //  this.theta[i] += 0.001* ( this.myData[i] -100) ;
+      this.theta[i] += 0.001* ( this.myData[i] -100) ;
       var x = this.theta[i];
       this.yvalues[i] = Math.sin(x) * 50;
        x += this.dx;
     }
    //console.log(this.yvalues);
     for (var i = 0; i <  this.yvalues.length  ; i++) {
-
         this.splineObject.geometry.vertices[i].y =  this.yvalues[i];
 
+        /*NOTHING HAPPENS */
+        this.tubeMesh.parent.curve.points[i].y = this.yvalues[i];
+
     }
+    //this.TubeGeometry.FrenetFrames(path, 256, false)
     //console.log(this.yvalues);
   }
 }
