e77889f4713f04d53f7bda4cef8ddd91e75dbd27
tournevis <arthur.ro92@gmail.com>
2016-02-28 15:58:13 +0100

    :snowflake: merci jojo :snowflake:

--- app/objects/spline.js
+++ app/objects/spline.js
@@ -6,14 +6,24 @@ export default class Spline extends THREE.Object3D {
     this.splice = innerWidth/256 ;
     this.decalX = innerWidth/2;
     this.points = [];
+    this.colors = [];
+    this.yvalues = new Float32Array(256);
+    this.theta = new Float32Array(256);
+    this.delta =0;
+    this.period =50;
+    this.dx = (2 * Math.PI / this.period) * this.splice ;
 
     for (var i = 0; i < 256; i++) {
       	this.points.push(new THREE.Vector3(this.splice * i - this.decalX , 0, -500 ));
+        this.colors[ i ] = new THREE.Color( 0xffffff );
+				this.colors[ i ].setHSL( 0.6, 1.0, Math.max( 0, i / 500 ) + 0.5 );
      }
+
     this.curve = new THREE.CatmullRomCurve3(this.points);
     this.geometry = new THREE.Geometry();
+    this.geometry.colors = this.colors;
     this.geometry.vertices = this.curve.getPoints( this.points.length-1 );
-    this.material = new THREE.LineBasicMaterial( { color : 0xff00f0, linewidth: 10, linecap: 'round' } );
+    this.material = new THREE.LineBasicMaterial({ color : 0x00FFBF, linewidth: 10, vertexColors: THREE.VertexColors });
 
     //Create the final Object3d to add to the scene
     this.splineObject = new THREE.Line( this.geometry, this.material );
@@ -30,9 +40,19 @@ export default class Spline extends THREE.Object3D {
   update() {
     this.splineObject.geometry.verticesNeedUpdate = true;
 
-    for (var i = 0; i < this.points.length ; i++) {
-      this.splineObject.geometry.vertices[i].y =  this.myData[i]*0.5;
-    }
 
+    for (var i = 0; i < this.myData.length; i++) {
+      //  this.theta[i] += 0.001* ( this.myData[i] -100) ;
+      var x = this.theta[i];
+      this.yvalues[i] = Math.sin(x) * 50;
+       x += this.dx;
+    }
+   //console.log(this.yvalues);
+    for (var i = 0; i <  this.yvalues.length  ; i++) {
+
+        this.splineObject.geometry.vertices[i].y =  this.yvalues[i];
+
+    }
+    //console.log(this.yvalues);
   }
 }
